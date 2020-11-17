3a2c9a7498d9dcd5b677fdf5928f209b211901d1
bweasels <benkumwes@gmail.com>
2020-11-17 02:18:31 +0000

    made a lil change to trigger login on cloud server

--- SetDirectory.R
+++ SetDirectory.R
@@ -8,6 +8,9 @@ setDirectory <- function(){
     outDir <- '/OneDrive/PhD/Fall 2020/Computational Genomics/data/'
     dataDir <- '/OneDrive/PhD/Fall 2020/Computational Genomics/scRNASeq-CNVCaller/Liver Cancer/Pt13.a/'
     plottingDir <- '/OneDrive/PhD/Fall 2020/Computational Genomics/scRNASeq-CNVCaller/plots/'
+
+    # This is my local directory on our google cloud server - you might have to set up your own if the user permissions don't play well
+    # or if you prefer your own filestructure
   }else if(info['user']=='bkw2118'){
     setwd('/home/bkw2118/scRNASeq-CNVCaller')
     outDir <- 'outDir/'
