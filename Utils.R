460eebff990c0616c9f82b59ff42c953e08ec663
Charlotte Rochereau <charlotte.rochereau@gmail.com>
2020-11-23 16:44:53 -0500

    added own local directories

--- Utils.R
+++ Utils.R
@@ -13,10 +13,16 @@ setDirectory <- function(){
     outDir <- '/OneDrive/PhD/Fall 2020/Computational Genomics/scRNASeq-CNVCaller/data/'
     dataDir <- '/OneDrive/PhD/Fall 2020/Computational Genomics/scRNASeq-CNVCaller/Liver Cancer/Pt13.a/'
     plottingDir <- '/OneDrive/PhD/Fall 2020/Computational Genomics/scRNASeq-CNVCaller/plots/'
+
+    }else if(info['user']=='charlotterochereau'){
+    #setwd('~/Code/scRNASeq-CNVCaller')
+    outDir <- 'outputs/'
+    dataDir <- 'raw_data/'
+    plottingDir <- 'plots/'
     
     # Turns out adding storage adds an external disk, so I had to mount it and move everything there
     # I think we both should have permissions to mess with it so I'll set it as the default
-  }else if(info['release']=='4.19.0-12-cloud-amd64'){
+    }else if(info['release']=='4.19.0-12-cloud-amd64'){
     setwd('/mnt/disks/ext/scRNASeq-CNVCaller')
     outDir <- '/mnt/disks/ext/scRNASeq-CNVCaller/output/'
     dataDir <- '/mnt/disks/ext/scRNASeq-CNVCaller/data/'
