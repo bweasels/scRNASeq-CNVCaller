dc97f6d270e91bd818db22472741e084bf0e9873
Charlotte Rochereau <charlotte.rochereau@gmail.com>
2020-11-24 15:29:31 -0500

    cleaned file

--- generateSplatterDatasets.R
+++ generateSplatterDatasets.R
@@ -17,18 +17,22 @@ date <- Sys.Date()
 #set.seed locks our random number indexer so we get repeatable results
 set.seed(10000)
 
-#data <- readRDS(paste0(outDir, 'dataFiltered_2020-11-15.RDS'))
 data <- readRDS(paste0(outDir, 'data_all.RDS'))
+geneLocs <- read.table(paste0(outDir, 'GeneLocs.txt'),
+                       sep = '\t',
+                       row.names = 1)
+# Trimming dataset to genes available in GeneLocs (from 19184 to 15252 genes)
+data <- data[rownames(data)[(rownames(data) %in% rownames(geneLocs))]]
 
 # Get essential stats for splatter simulation
-# 11K Cells took ~1.5 hours on my laptop
-if (!file.exists(paste0(outDir, 'InitialParams_2020-11-16.RDS'))) {
+if (!file.exists(paste0(outDir, 'InitialParams_2020-11-24.RDS'))) {
   params <- splatEstimate(as.matrix(data@assays$RNA@counts))
   saveRDS(params, paste0('InitialParams_', date, '.RDS'))
 } else {
-  params <- readRDS('InitialParams_2020-11-16.RDS')
+  params <- readRDS('InitialParams_2020-11-24.RDS')
 }
 
+# params: ngenes = 15252, ncells = 5278
 # Need to figure out parameters to mock cnv variations
 sim <- splatSimulate(params)
 saveRDS(sim, paste0(outDir, 'data_sim.RDS'))
