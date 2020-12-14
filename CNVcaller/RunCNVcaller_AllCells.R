1ea6011b98ff8bec32f85ca1e743a55231e97dca
Ben Wesley <benkumwes@gmail.com>
2020-12-13 19:30:01 -0500

    Minor performance improvement

--- CNVcaller/RunCNVcaller_AllCells.R
+++ CNVcaller/RunCNVcaller_AllCells.R
@@ -89,6 +89,6 @@ infercnv_obj <- run_no_smoothing(infercnv_obj,
                                  minExprRatio = minRat,
                                  maxExprRatio = maxRat,
                                  pThresh = pThresh,
-                                 diagnostics = F,
+                                 diagnostics = T,
                                  resume_mode = F)
 print(paste("Finished P Thresh:", pThresh))
